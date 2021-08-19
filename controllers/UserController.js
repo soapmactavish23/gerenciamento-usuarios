d69da9ad2ec8507e208c0b37ed1b134138b47d2c
HKprogram <ricknogueira1231@gmail.com>
2021-08-19 18:13:05 -0300

    Trabalhando com edição de dados

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,7 +6,25 @@ class UserController {
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit();
-        this.updateCount();
+        this.onEditCancel();
+    }
+
+    onEditCancel() {
+
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e => {
+            this.showPanelCreate();
+        });
+
+    }
+
+    showPanelCreate() {
+        document.querySelector("#box-user-create").style.display = "block";
+        document.querySelector("#box-user-update").style.display = "none";
+    }
+
+    showPanelUpdate() {
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
     }
 
     onSubmit() {
@@ -117,9 +135,14 @@ class UserController {
             <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>`;
+
+        tr.querySelector(".btn-edit").addEventListener("click", e => {
+            console.log(JSON.parse(tr.dataset.user));
+            this.showPanelUpdate();
+        });
 
         this.tableEl.appendChild(tr);
 
@@ -128,7 +151,7 @@ class UserController {
     }
 
     updateCount() {
-        
+
         let numberUsers = 0;
         let numberAdmin = 0;
 
@@ -137,14 +160,14 @@ class UserController {
             numberUsers++;
 
             let user = JSON.parse(tr.dataset.user)
-            
-            if(user._admin) numberAdmin++;
+
+            if (user._admin) numberAdmin++;
 
         });
 
         document.querySelector("#number-users").innerHTML = numberUsers;
         document.querySelector("#number-users-admin").innerHTML = numberAdmin;
-        
+
 
     }
 
