fa6f1504e7a92e2368b19cbba6cb3dea8d280592
HKprogram <ricknogueira1231@gmail.com>
2021-08-19 17:54:17 -0300

    Usando dataset

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,7 +6,7 @@ class UserController {
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit();
-
+        this.updateCount();
     }
 
     onSubmit() {
@@ -14,14 +14,19 @@ class UserController {
             event.preventDefault();
 
             let btn = this.formEl.querySelector("[type=submit]");
-            
+
             btn.disabled = true;
 
             let values = this.getValues();
 
+            if (!values) {
+                btn.disabled = false;
+                return false;
+            }
+
             this.getPhoto().then((photo) => {
                 values.photo = photo;
-                
+
                 this.addLine(values);
 
                 this.formEl.reset();
@@ -66,7 +71,18 @@ class UserController {
 
         let user = {};
 
+        let isValid = true;
+
         [...this.formEl.elements].forEach(function (field, index) {
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+                return false;
+
+            }
+
             if (field.name == "gender") {
                 if (field.checked) {
                     user[field.name] = field.value;
@@ -80,6 +96,10 @@ class UserController {
             }
         });
 
+        if (!isValid) {
+            return false;
+        }
+
         return new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
 
     }
@@ -88,18 +108,44 @@ class UserController {
 
         let tr = document.createElement('tr');
 
+        tr.dataset.user = JSON.stringify(dataUser);
+
         tr.innerHTML =
             `<td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
-            <td>${dataUser.register}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>`;
 
         this.tableEl.appendChild(tr);
+
+        this.updateCount();
+
+    }
+
+    updateCount() {
+        
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr => {
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user)
+            
+            if(user._admin) numberAdmin++;
+
+        });
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+        
+
     }
 
 }
