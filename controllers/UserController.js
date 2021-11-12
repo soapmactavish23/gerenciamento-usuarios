25619263b26e05bb3652ac5d99b537834d1d0adf
HKprogram <ricknogueira1231@gmail.com>
2021-11-12 18:37:27 -0300

    Excluindo dados do objeto

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -43,7 +43,7 @@ class UserController {
 
                     if (!values.photo) {
                         result._photo = userOld._photo;
-                    }else {
+                    } else {
                         result._photo = content;
                     }
 
@@ -220,7 +220,7 @@ class UserController {
                 <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
                     <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                    <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
                 </td>
             </tr>
         `;
@@ -234,6 +234,13 @@ class UserController {
     }
 
     addEventsTr(tr) {
+        tr.querySelector(".btn-delete").addEventListener("click", e => {
+            if(confirm("Deseja realmente excluir?")){
+                tr.remove();
+                this.updateCount();
+            }
+        });
+
         tr.querySelector(".btn-edit").addEventListener("click", e => {
 
             let json = JSON.parse(tr.dataset.user);
