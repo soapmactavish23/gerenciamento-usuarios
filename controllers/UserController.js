221bc17a3a1ec2759c209a7e0fdac55a4db1efb8
soapmactavish23 <ricknogueira1231@gmail.com>
2021-11-13 21:54:55 -0300

    Trabalhando com localStorage - Editando Dados no localStorage

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -52,6 +52,8 @@ class UserController {
 
                     user.loadFromJSON(result);
 
+                    user.save();
+
                     this.getTr(user, tr);
 
                     this.updateCount();
@@ -91,7 +93,7 @@ class UserController {
 
                     values.photo = content;
 
-                    this.insert(values);
+                    values.save();
 
                     this.addLine(values);
 
@@ -216,16 +218,6 @@ class UserController {
 
             this.addLine(user);
         });
-    }
-
-    insert(data) {
-
-        let users = this.getUsersStorage();
-
-        users.push(data);
-
-        localStorage.setItem("users", JSON.stringify(users));
-
     }
 
     addLine(dataUser) {
--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ class User {
 
     constructor(name, gender, birth, country, email, password, photo, admin) {
 
+        this._id;
         this._name = name;
         this._gender = gender;
         this._birth = birth;
@@ -12,6 +13,10 @@ class User {
         this._admin = admin;
         this._register = new Date();
 
+    }
+
+    get id() {
+        return this._id;
     }
 
     get register() {
@@ -58,6 +63,46 @@ class User {
                     break;
             }
         }
+    }
+
+    getNewID() {
+
+        if (!window.id) window.id = 0;
+
+        id++;
+
+        return id;
+
+    }
+
+    getUsersStorage() {
+        let users = [];
+
+        if (localStorage.getItem("users")) {
+            users = JSON.parse(localStorage.getItem("users"));
+        }
+
+        return users;
+    }
+
+    save() {
+        
+        let users = this.getUsersStorage();
+
+        if (this.id > 0) {
+            users.map(u => {
+                if (u._id === this.id) {
+                    u = this;
+                }
+                return u;
+            });
+        } else {
+            this._id = this.getNewID();
+            users.push(this);
+        }
+
+        localStorage.setItem("users", JSON.stringify(users));
+
     }
 
 }
